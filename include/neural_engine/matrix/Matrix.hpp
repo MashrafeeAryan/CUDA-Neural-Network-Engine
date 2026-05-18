9ca073cbf5825620fc8a8a0ba7964832913812be
Mashrfee Aryan <aryanmashrafee07@gmail.com>
2026-05-17 21:23:52 -0500

    added index thing

--- include/neural_engine/matrix/Matrix.hpp
+++ include/neural_engine/matrix/Matrix.hpp
@@ -10,6 +10,13 @@ class Matrix{
         int rows, cols;
 
         //declare vector 
+        //vector is a dynamic array
+        //std means standard. It is a namespace
+        //Namespace is the name of protective box that library's tools are packed inside
+        //Scope Resolution Operator - ::
+            //Tells compiler where to look for a tool
+        std::vector<double> data;
+
     public:
         // Constructor: function inside class that runs automatically 
         // when we create an object
@@ -21,7 +28,24 @@ class Matrix{
         // I am using member initializer lists
         // Instead of creating rows and cols first, then assign r and c into them,
         // create rows with r and c immediately
+        // Initialized data vector with 0.0
         Matrix(int r, int c)
-            :rows(r), cols(c){
+            :rows(r), cols(c), data(r * c, 0.0){
         }
+
+        // So this is how our vector will look like
+        // Matrix(2,3) = [0,0,0,0,0,0]
+        // Actual Matrix:
+        //        [ 0,0,0
+        //          0,0,0]
+        // Now to access each element of vector we use row
+        // How the index works
+            // A(1,2) You want to access first row second element
+            // index = element_searching_row * number of columns + element_searching_column
+            // index = 1 * 3 + 2 = 5
+
+        double& operator()(int r, int c){
+            return data[ r * cols + c];
+        }
+
 };
